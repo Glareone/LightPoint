e14b7ff387cd131e3e4fcc2bca6f981a8e7e86c9
Glareone <enlarius@gmail.com>
2015-03-20 12:44:29 +0300

    global.asax update

--- LightPoint.ASPMVC/Global.asax.cs
+++ LightPoint.ASPMVC/Global.asax.cs
@@ -6,9 +6,13 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using LightPoint.Domain.Services;
+using LightPoint.Service;
+using LightPoint.ServiceLocator;
 namespace LightPoint.ASPMVC
 {
+
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -18,6 +22,22 @@ namespace LightPoint.ASPMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Register Existing Services
+            ServiceLocator.ServiceLocator.RegisterService<IShopService>(typeof(ShopService));
+            ServiceLocator.ServiceLocator.RegisterService<IGoodsService>(typeof(GoodsService));
+
+            ////Configuration of Database 
+            ////Work with file database, uncomment if need and comment the SQL factory.
+            ////Data.Context.Current.RepositoryFactory = new RepositoryFactory();
+
+            //UnitOfWork.Context.Current.UnitOfWorkFactory = new FileSystemDatabaseUnitOfWorkFactory();
+
+            //Configuration of Database
+            //Work with SQL Database,if need work with file database need to comment Factory;
+            Data.Context.Current.RepositoryFactory = new Data.SQL.RepositoryFactory();
+
+            UnitOfWork.Context.Current.UnitOfWorkFactory = new Data.SQL.SQLSystemUnitOfWork.SQLSystemDatabaseUnitofWorkFactory();
         }
     }
 }
